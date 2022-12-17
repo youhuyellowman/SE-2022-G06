7b42121025b8943dafc9b56f71378cd39a0e2298
BaSO4 <2497212385@qq.com>
2022-12-18 07:59:10 +0800

    邹雨哲part

--- program/鹰眼反应力/app.js
+++ program/鹰眼反应力/app.js
@@ -1,2 +1,16 @@
 // app.js
-App({})
+App({
+  onLaunch(){
+    wx.cloud.init({
+      env:{
+        database:'test-0gzpu617280360cb',
+        storage:'test-0gzpu617280360cb',//储存API默认环境
+        functions:'test-0gzpu617280360cb'//云函数API默认环境
+      },
+      traceUser: true
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
