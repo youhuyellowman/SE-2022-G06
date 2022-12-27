6aeacabeab9c05df9208f10ddc0c915e282e6b55
yaojiesheng <1139062052@qq.com>
2022-12-27 22:50:41 +0800

    1

--- program/鹰眼反应力/app.js
+++ program/鹰眼反应力/app.js
@@ -1,11 +1,11 @@
 // app.js
-App({
+// App({
 
   // onLaunch: function(){
-  //   //云开发环境的初始化
-  //   wx.cloud.init({
-  //     env:"cloud1-4gsh9n5vd3aab145"
-  //   })
+    //云开发环境的初始化
+    // wx.cloud.init({
+    //   env:"cloud1-4gsh9n5vd3aab145"
+    // })
   // onLaunch(){
   //   wx.cloud.init({
   //     env:{
@@ -18,5 +18,5 @@ App({
   // }
   // globalData: {
   //   userInfo: null
-  // }
-})
+//   }
+// })
