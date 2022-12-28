e0be190baf6374777981755e260b29d64d700c2d
yaojiesheng <1139062052@qq.com>
2022-12-28 11:26:14 +0800

    测试

--- program/鹰眼反应力/pages/runtest/runtest.js
+++ program/鹰眼反应力/pages/runtest/runtest.js
@@ -1,14 +1,20 @@
 // index.js
 // 获取应用实例
 const app = getApp()
-let firsttest = 0;//上次点击时间
+let firsttest = 0; //上次点击时间
 let firstchange = 0;
 let ftesttime = 0;
-let clicknum=0;//点击次数默认0
+let clicknum = 0; //点击次数默认0
+let timefirst = 0;
+let timesecond = 0;
+let timethird = 0;
 Page({
   data: {
     motto: 'Hello World',
-    color1:'red',
+    color1: 'red',
+    first:'',
+    second:'',
+    third:'',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -16,34 +22,69 @@ Page({
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
 
-	click() {//按钮方法
-		//let currentTime = Date.now();//设置本次点击时间
+  click() { //按钮方法
+    //let currentTime = Date.now();//设置本次点击时间
     //let intervaldate = currentTime - clicktime;//本次点击时间减上次点击时间等于间隔时间
-    var date=new Date();
-    clicknum=clicknum+1;
+    var date = new Date();
+    clicknum = clicknum + 1;
     console.log(clicknum)
-    if(clicknum%2==1){
-      this.setData({color1:'blue'})
-      var randNum =  (Math.random() * 8000) + 2000;
+    if (clicknum % 2 == 1) {
+      this.setData({
+        color1: 'blue'
+      })
+      var randNum = (Math.random() * 8000) + 2000;
       console.log(randNum)
-      setTimeout(()=>{
-          this.setData({color1:'green'})
-        },randNum)
-        let firstchange=date.getMilliseconds();  		
-  }
-  if(clicknum%2==0){
-    let firsttest = date.getMilliseconds();;
-    let ftesttime=firsttest-firstchange
-    console.log(ftesttime)
-  }
-		//clicktime = currentTime;//设置上次点击时间
-	  },
-  torank(){
+      setTimeout(() => {
+        this.setData({
+          color1: 'green'
+        })
+      }, randNum)
+      let firstchange = date.getMilliseconds();
+    }
+    if (clicknum % 2 == 0) {
+      let firsttest = date.getMilliseconds();
+      let timecj = firsttest - firstchange
+      if (timecj < 100) {
+        timecj = timecj + 1000
+      }
+      console.log(timecj)
+      if (clicknum == 2) {
+        timefirst = timecj
+      }
+      if (clicknum == 4) {
+        timesecond = timecj
+      }
+      if (clicknum == 6) {
+        timethird = timecj
+        // console.log(timefirst)
+        // console.log(timesecond)
+        // console.log(timethird)
+      }
+
+    }
+    //clicktime = currentTime;//设置上次点击时间
+  },
+  firstshow:function () {
+    this.setData({
+      first:timefirst
+    })
+  },
+  secondshow:function () {
+    this.setData({
+      second:timesecond
+    })
+  },
+  thirdshow:function () {
+    this.setData({
+      third:timethird
+    })
+  },
+  torank() {
     wx.navigateTo({
       url: '/pages/rank/rank',
     })
   },
-  tomy(){
+  tomy() {
     wx.navigateTo({
       url: '/pages/myinfo/myinfo',
     })
